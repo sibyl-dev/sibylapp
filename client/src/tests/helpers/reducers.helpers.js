fd4e312cb3e6b7fc406fa5fb60bd6e3c9f0ef752
Radu Ionut-Alexandru <radui1398@gmail.com>
2020-09-29 15:50:49 +0300

    fix: helpers description

--- client/src/tests/helpers/reducers.helpers.js
+++ client/src/tests/helpers/reducers.helpers.js
@@ -1,3 +1,26 @@
+/*
+    Helper to generate test for promise reducers. A promise reducer is called by an action that looks like this:
+    dispatch({
+    type: 'ACTION_NAME',
+    promise: '<api call>,
+    })
+
+    Test with this function: ACTION_NAME_REQUEST, ACTION_NAME_SUCCESS, ACTION_NAME_FAILURE
+
+    ARGS:
+    reducer: Reducer,
+    defaultState: The initial state,
+    promiseReducers: [
+      {
+        name: '<ACTION_NAME>',
+        loading: '<dataIsLoadingKey>',
+        key: '<dataKey>',
+        sendData: <dataSentOnSuccess>,
+        testData: <testDataAfterReducer>,
+        failData: <testDataWhenCallFails>,
+      },
+    ]
+ */
 export const generateTestsForPromiseReducers = (reducer, defaultState, promiseReducers) => {
   describe('Reducers triggered by actions with promise', () => {
     promiseReducers.forEach((promiseReducer) => {
@@ -39,6 +62,23 @@ export const generateTestsForPromiseReducers = (reducer, defaultState, promiseRe
   });
 };
 
+/*
+    Helper to generate tests for set reducers. A set reducer is used just to set
+    one value and the sent data must have the same key as the key saved in the store.
+
+    ARGS:
+    reducer: Reducer,
+    defaultState: The initial state,
+    setReducers: [
+      {
+        name: '<ACTION_NAME>',
+        description: '<actionDescription>',
+        key: '<dataKey>',
+        sendData: '<dataToSend>',
+        testData: '<dataToTest>',
+      },
+    ]
+ */
 export const generateTestsForSetReducers = (reducer, defaultState, setReducers) => {
   describe('Reducers that set only one value', () => {
     setReducers.forEach((setReducer) => {
